3566ce497bfdbfb978b17afeb6606eab5ac9ac02
eager-loop-4095 <jingchenjc2019@gmail.com>
2019-01-22 10:20:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ function nowServing(line){
   if (line.length===0) {
     return "There is nobody waiting to be served!";
   } else {
-    return "Currently serving " + "number" + line.shift() +".";
+    return "Currently serving " + line.shift() +".";
   }
   return line;
 }

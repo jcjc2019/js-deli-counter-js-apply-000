27483abbaa3de78e9655e86db561672c9245f4ad
eager-loop-4095 <jingchenjc2019@gmail.com>
2019-01-22 10:08:12 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 function takeANumber(line,PersonName) {
-    line.push(PersonName); //return length of line
+    //line.push(PersonName); //return length of line
     //var position = line.indexOf(PersonName) + 1;
     //line.length
-    return "Welcome, " + PersonName + ". You are number "+ line.push(PersonName) - 1 +" in line.";
+    return "Welcome, " + PersonName + ". You are number "+ line.push(PersonName) +" in line.";
 }
 
 function nowServing(line){
